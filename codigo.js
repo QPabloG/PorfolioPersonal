f7df9748367d1ab50b786003e316b259864c4906
QPabloG <105458632+QPabloG@users.noreply.github.com>
2025-09-20 22:52:23 -0300

    Primera Subida
    Subida de Porfolio

---
+++ codigo.js
@@ -0,0 +1,15 @@
+
+        function saludar(){
+            var user = document.getElementById("txtUsername").value;
+            var pass = document.getElementById("txtPassword").value;
+            alert("Te damos la bienvenida " + user);
+            console.log("tu password no es correcto" + pass);
+        }
+        
+        function sumar(){
+            var n1 = parseInt(document.getElementById("txtUsername").value);
+            var n2 = parseInt(document.getElementById("txtPassword").value);
+            alert("La suma es:  " + (n1 + n2));
+        }
+
+   
